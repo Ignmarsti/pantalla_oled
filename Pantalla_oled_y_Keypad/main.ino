148734a3e273fb69e6210ab4c76271b7c7ba247d
Nachettt <stiefelignacio@hotmail.com>
2021-02-11 12:40:49 +0100

    He resuelto todos los problemas. Una vez entraba en uno de lo bucles no salía de el nunca porque no usaba la instrucción break, lo que implicaba que mi variable i tampoco recuperase su valor y esto me llevaba a realizar mal el bucle for. Ya no acepta entrada de horas que no sean las correctas, ni de caracteres.

--- Pantalla_oled_y_Keypad/main.ino
+++ Pantalla_oled_y_Keypad/main.ino
@@ -22,9 +22,19 @@ char tecla_pulsada;
 char horariego1[6];
 char horariego2[6];
 char horariego3[6];
+
 int i=0;
+int tecla_numerica;
+int tecla_anterior;
 char opcion_seleccionada;
+bool bandera;
 bool bandera_seleccion;
+
+void menu_principal();
+void opcion_a();
+void opcion_b();
+void opcion_c();
+void error_numerico();
  
 // Objeto de la clase Adafruit_SSD1306
 Adafruit_SSD1306 display(ANCHO_PANTALLA, ALTO_PANTALLA, &Wire, -1);//&Wire es un puntero de la clase estática Wire. -1 es el pin de Arduino o ESP8266 que se utiliza para resetear la pantalla en caso de que la pantalla tenga un pin RST (no es nuestro caso)
@@ -32,12 +42,271 @@ Keypad teclado = Keypad(makeKeymap(teclas), pinesFilas, pinesColumnas, filas, co
  
 void setup() {
 
-  Serial.begin(9600);//Inciamos la comunicación serial a 9600 baudios  
-  delay(100);//esperamos que se inicie
   bandera_seleccion=0;//Bajamos la bandera de selección
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);//Iniciamos la pantalla OLED que se encuentra en la dirección 0x3C
+
+  
+  menu_principal();
+
  
-  // Limpiar buffer
+}
+ 
+void loop() {
+
+    tecla_pulsada = teclado.getKey();//Comprobamos que tecla se ha pulsado
+    opcion_seleccionada=tecla_pulsada;//La guardamos en una nueva variable
+    //Tenemos varias opciones
+    if(opcion_seleccionada == 'A'){
+      opcion_a();
+      bandera_seleccion=1;
+    }
+    if(opcion_seleccionada == 'B'){
+      opcion_b();
+      bandera_seleccion=1;
+    }
+    if(opcion_seleccionada == 'C'){
+      opcion_c();
+      bandera_seleccion=1;
+    }
+
+
+}
+
+//Aquí tenemos las distintas funciones en las que podemos programar la hora de riego
+void opcion_a(){
+    if(bandera_seleccion==1){
+
+
+    //Limpiamos el display, situamos el cursor en la posición adecuada e imprimimos por pantalla el mensaje
+    display.clearDisplay();
+    display.setCursor(0,0);
+    display.println("Opcion A seleccionada");
+    display.display();
+
+    //Creamos un bucle en el que cada vez que recibimos una tecla, la guardamos en el vector corresponiente
+    for(i=0;i<6;i++){
+      tecla_pulsada=teclado.waitForKey();////Esta función es importante, ya que hasta que no pulsemos una tecla, el micro se queda parado en está instrucción. La unica manera de avanzar es pulsar una tecla o salir de bucle for
+      if(tecla_pulsada=='A'||tecla_pulsada=='B'||tecla_pulsada=='C'||tecla_pulsada=='D'||tecla_pulsada=='*'||tecla_pulsada=='#'){
+        display.clearDisplay();
+        display.setCursor(0,0);
+        display.println("ERROR");
+        display.setCursor(0,10);
+        display.println("La tecla pulsada no es numerica");
+        display.display();
+        delay(2000);
+        bandera_seleccion=0;
+        menu_principal();
+        break;
+      }
+      else{
+        horariego1[i]=tecla_pulsada;
+        tecla_numerica=horariego1[i]-48;
+        if(i==0&&tecla_numerica>2){          
+          error_numerico();
+          menu_principal();
+          break;
+        }
+        if(i==0&&tecla_numerica==2){
+          tecla_anterior=tecla_numerica;
+        }
+        if(i==1&&tecla_anterior==2&&tecla_numerica>4){
+          error_numerico();
+          menu_principal();
+          break;
+        }
+        if(i==2&&tecla_numerica>5){
+          error_numerico();
+          menu_principal();
+          break;
+        }
+        if(i==4&&tecla_numerica>5){
+          error_numerico();
+          menu_principal();
+          break;
+        }
+        
+        if(i==5){//Si ya hemos terminado de meter la hora completa, esta ocupará 6 espacios, por lo que una vez estén rellenos imprimiremos por pantalla lo que tenemos en el vector
+          display.clearDisplay();
+          display.setCursor(3,0);  
+          display.println("Se ha programado la hora de riego a las ");
+          display.write(horariego1[0]);
+          display.write(horariego1[1]);
+          display.print(':');
+          display.write(horariego1[2]);
+          display.write(horariego1[3]);
+          display.print(':');
+          display.write(horariego1[4]);
+          display.write(horariego1[5]); 
+          display.display();
+          delay(5000);
+          display.clearDisplay();
+          menu_principal();
+          break;
+         }
+    }
+    }
+    }
+}
+
+//Aquí tenemos las distintas funciones en las que podemos programar la hora de riego
+void opcion_b(){
+    if(bandera_seleccion==1){
+    bandera_seleccion=0;//Lo primero que hacemos es bajar la bandera de seleccion
+
+    //Limpiamos el display, situamos el cursor en la posición adecuada e imprimimos por pantalla el mensaje
+    display.clearDisplay();
+    display.setCursor(0,0);
+    display.println("Opcion B seleccionada");
+    display.display();
+
+    //Creamos un bucle en el que cada vez que recibimos una tecla, la guardamos en el vector corresponiente
+    for(i=0;i<6;i++){
+      tecla_pulsada=teclado.waitForKey();////Esta función es importante, ya que hasta que no pulsemos una tecla, el micro se queda parado en está instrucción. La unica manera de avanzar es pulsar una tecla o salir de bucle for
+      if(tecla_pulsada=='A'||tecla_pulsada=='B'||tecla_pulsada=='C'||tecla_pulsada=='D'||tecla_pulsada=='*'||tecla_pulsada=='#'){
+        display.clearDisplay();
+        display.setCursor(0,0);
+        display.println("ERROR");
+        display.setCursor(0,10);
+        display.println("La tecla pulsada no es numerica");
+        display.display();
+        delay(2000);
+        menu_principal();
+        break;
+      }
+      else{
+        horariego1[i]=tecla_pulsada;
+        tecla_numerica=horariego1[i]-48;
+        if(i==0&&tecla_numerica>2){          
+          error_numerico();
+          menu_principal();
+          break;
+        }
+        if(i==0&&tecla_numerica==2){
+          tecla_anterior=tecla_numerica;
+        }
+        if(i==1&&tecla_anterior==2&&tecla_numerica>4){
+          error_numerico();
+          menu_principal();
+          break;
+        }
+        if(i==2&&tecla_numerica>5){
+          error_numerico();
+          menu_principal();
+          break;
+        }
+        if(i==4&&tecla_numerica>5){
+          error_numerico();
+          menu_principal();
+          break;
+        }
+      horariego2[i]=tecla_pulsada;
+      if(i==5){//Si ya hemos terminado de meter la hora completa, esta ocupará 6 espacios, por lo que una vez estén rellenos imprimiremos por pantalla lo que tenemos en el vector
+        display.clearDisplay();
+        display.setCursor(3,0);  
+        display.println("Se ha programado la hora de riego a las ");
+        display.write(horariego2[0]);
+        display.write(horariego2[1]);
+        display.print(':');
+        display.write(horariego2[2]);
+        display.write(horariego2[3]);
+        display.print(':');
+        display.write(horariego2[4]);
+        display.write(horariego2[5]); 
+        display.display();
+        delay(5000);
+        display.clearDisplay();
+        menu_principal();
+        break;
+      }
+    }
+    
+
+    }
+}
+}
+
+//Aquí tenemos las distintas funciones en las que podemos programar la hora de riego
+void opcion_c(){
+    if(bandera_seleccion==1){
+    bandera_seleccion=0;//Lo primero que hacemos es bajar la bandera de seleccion
+
+    //Limpiamos el display, situamos el cursor en la posición adecuada e imprimimos por pantalla el mensaje
+    display.clearDisplay();
+    display.setCursor(0,0);
+    display.println("Opcion C seleccionada");
+    display.display();
+
+    //Creamos un bucle en el que cada vez que recibimos una tecla, la guardamos en el vector corresponiente
+    for(i=0;i<6;i++){
+      tecla_pulsada=teclado.waitForKey();////Esta función es importante, ya que hasta que no pulsemos una tecla, el micro se queda parado en está instrucción. La unica manera de avanzar es pulsar una tecla o salir de bucle for
+      if(tecla_pulsada=='A'||tecla_pulsada=='B'||tecla_pulsada=='C'||tecla_pulsada=='D'||tecla_pulsada=='*'||tecla_pulsada=='#'){
+        display.clearDisplay();
+        display.setCursor(0,0);
+        display.println("ERROR");
+        display.setCursor(0,10);
+        display.println("La tecla pulsada no es numerica");
+        display.display();
+        delay(2000);
+        menu_principal();
+        break;
+      }
+      else{
+        horariego1[i]=tecla_pulsada;
+        tecla_numerica=horariego1[i]-48;
+        if(i==0&&tecla_numerica>2){          
+          error_numerico();
+          menu_principal();
+          break;
+        }
+        if(i==0&&tecla_numerica==2){
+          tecla_anterior=tecla_numerica;
+        }
+        if(i==1&&tecla_anterior==2&&tecla_numerica>4){
+          error_numerico();
+          menu_principal();
+          break;
+        }
+        if(i==2&&tecla_numerica>5){
+          error_numerico();
+          menu_principal();
+          break;
+        }
+        if(i==4&&tecla_numerica>5){
+          error_numerico();
+          menu_principal();
+          break;
+        }
+      if(i==5){//Si ya hemos terminado de meter la hora completa, esta ocupará 6 espacios, por lo que una vez estén rellenos imprimiremos por pantalla lo que tenemos en el vector
+        display.clearDisplay();
+        display.setCursor(3,0);  
+        display.println("Se ha programado la hora de riego a las ");
+        display.write(horariego3[0]);
+        display.write(horariego3[1]);
+        display.print(':');
+        display.write(horariego3[2]);
+        display.write(horariego3[3]);
+        display.print(':');
+        display.write(horariego3[4]);
+        display.write(horariego3[5]);
+        display.display();
+        delay(5000);
+        display.clearDisplay();
+        menu_principal();
+        break;
+      }
+      
+    }
+    
+
+    }
+}
+}
+
+void menu_principal(){
+
+  i=0;
+
+    // Limpiar buffer
   display.clearDisplay();
  
   // Tamaño del texto
@@ -66,130 +335,13 @@ void setup() {
   display.setCursor(0,0);
   display.println("Pulse A, B y C para  cambiar las horas de riego 1, 2 y 3 respectivamente");
   display.display();
-  
- 
-}
- 
-void loop() {
-  tecla_pulsada = teclado.getKey();//Comprobamos que tecla se ha pulsado
-  opcion_seleccionada=tecla_pulsada;//La guardamos en una nueva variable
-  //Tenemos varias opciones
-  if(opcion_seleccionada == 'A'){
-    opcion_a();
-    bandera_seleccion=1;
-  }
-  if(opcion_seleccionada == 'B'){
-    opcion_b();
-    bandera_seleccion=1;
-  }
-  if(opcion_seleccionada == 'C'){
-    opcion_c();
-    bandera_seleccion=1;
-  }
-
 }
 
-//Aquí tenemos las distintas funciones en las que podemos programar la hora de riego
-void opcion_a(){
-    if(bandera_seleccion==1){
-    bandera_seleccion=0;//Lo primero que hacemos es bajar la bandera de seleccion
 
-    //Limpiamos el display, situamos el cursor en la posición adecuada e imprimimos por pantalla el mensaje
-    display.clearDisplay();
-    display.setCursor(0,0);
-    display.println("Opcion A seleccionada");
-    display.display();
-
-    //Creamos un bucle en el que cada vez que recibimos una tecla, la guardamos en el vector corresponiente
-    for(i=0;i<6;i++){
-      tecla_pulsada=teclado.waitForKey();////Esta función es importante, ya que hasta que no pulsemos una tecla, el micro se queda parado en está instrucción. La unica manera de avanzar es pulsar una tecla o salir de bucle for
-      horariego1[i]=tecla_pulsada;
-      if(i==5){//Si ya hemos terminado de meter la hora completa, esta ocupará 6 espacios, por lo que una vez estén rellenos imprimiremos por pantalla lo que tenemos en el vector
-        display.clearDisplay();
-        display.setCursor(15,0);  
-        display.print("Se ha programado la hora de riego a las ");
-        display.write(horariego1[0]);
-        display.write(horariego1[1]);
-        display.print(':');
-        display.write(horariego1[2]);
-        display.write(horariego1[3]);
-        display.print(':');
-        display.write(horariego1[4]);
-        display.write(horariego1[5]); 
-        display.display();
-      }
-    }
-    
-
-    }
-}
-
-//Aquí tenemos las distintas funciones en las que podemos programar la hora de riego
-void opcion_b(){
-    if(bandera_seleccion==1){
-    bandera_seleccion=0;//Lo primero que hacemos es bajar la bandera de seleccion
-
-    //Limpiamos el display, situamos el cursor en la posición adecuada e imprimimos por pantalla el mensaje
-    display.clearDisplay();
-    display.setCursor(0,0);
-    display.println("Opcion B seleccionada");
-    display.display();
-
-    //Creamos un bucle en el que cada vez que recibimos una tecla, la guardamos en el vector corresponiente
-    for(i=0;i<6;i++){
-      tecla_pulsada=teclado.waitForKey();////Esta función es importante, ya que hasta que no pulsemos una tecla, el micro se queda parado en está instrucción. La unica manera de avanzar es pulsar una tecla o salir de bucle for
-      horariego2[i]=tecla_pulsada;
-      if(i==5){//Si ya hemos terminado de meter la hora completa, esta ocupará 6 espacios, por lo que una vez estén rellenos imprimiremos por pantalla lo que tenemos en el vector
-        display.clearDisplay();
-        display.setCursor(15,0);  
-        display.print("Se ha programado la hora de riego a las ");
-        display.write(horariego2[0]);
-        display.write(horariego2[1]);
-        display.print(':');
-        display.write(horariego2[2]);
-        display.write(horariego2[3]);
-        display.print(':');
-        display.write(horariego2[4]);
-        display.write(horariego2[5]); 
-        display.display();
-      }
-    }
-    
-
-    }
-}
-
-//Aquí tenemos las distintas funciones en las que podemos programar la hora de riego
-void opcion_c(){
-    if(bandera_seleccion==1){
-    bandera_seleccion=0;//Lo primero que hacemos es bajar la bandera de seleccion
-
-    //Limpiamos el display, situamos el cursor en la posición adecuada e imprimimos por pantalla el mensaje
-    display.clearDisplay();
-    display.setCursor(0,0);
-    display.println("Opcion C seleccionada");
-    display.display();
-
-    //Creamos un bucle en el que cada vez que recibimos una tecla, la guardamos en el vector corresponiente
-    for(i=0;i<6;i++){
-      tecla_pulsada=teclado.waitForKey();////Esta función es importante, ya que hasta que no pulsemos una tecla, el micro se queda parado en está instrucción. La unica manera de avanzar es pulsar una tecla o salir de bucle for
-      horariego3[i]=tecla_pulsada;
-      if(i==5){//Si ya hemos terminado de meter la hora completa, esta ocupará 6 espacios, por lo que una vez estén rellenos imprimiremos por pantalla lo que tenemos en el vector
-        display.clearDisplay();
-        display.setCursor(15,0);  
-        display.print("Se ha programado la hora de riego a las ");
-        display.write(horariego3[0]);
-        display.write(horariego3[1]);
-        display.print(':');
-        display.write(horariego3[2]);
-        display.write(horariego3[3]);
-        display.print(':');
-        display.write(horariego3[4]);
-        display.write(horariego3[5]); 
-        display.display();
-      }
-    }
-    
-
-    }
+void error_numerico(){
+  display.clearDisplay();
+  display.setCursor(0,0);
+  display.println("La hora introducida no es valida");
+  display.display();
+  delay(2000);
 }
